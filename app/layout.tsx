49c2559ff634ee2f65b24de4abf6205aebf5bdb0
quyetbd <bquyet09@gmail.com>
2026-04-09 09:36:23 +0700

    [Fix]: fix conflict

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import { AntdRegistry } from '@ant-design/nextjs-registry'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/lib/auth'
-import { Providers } from '@/app/components/Providers'
-import { Inter } from "next/font/google"
+import type { Metadata } from 'next';
+import './globals.css';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+import { Providers } from '@/app/components/Providers';
+import { Inter } from 'next/font/google';
+import { ConfigProvider } from 'antd';
+import { themeConfig } from '@/lib/utils/colors';
+import enUS from 'antd/locale/en_US';
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Sky Finance',
@@ -24,9 +27,17 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AntdRegistry>
-          <Providers session={session}>
-            {children}
-          </Providers>
+          <ConfigProvider
+            locale={enUS}
+            theme={{
+              token: {
+                ...themeConfig.token,
+                fontFamily: 'inherit',
+              },
+            }}
+          >
+            <Providers session={session}>{children}</Providers>
+          </ConfigProvider>
         </AntdRegistry>
       </body>
     </html>
